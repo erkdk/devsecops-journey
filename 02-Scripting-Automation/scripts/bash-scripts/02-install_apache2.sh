576fce171ec336dd62806b4c9125b22a93f9d983
Aadarsha Khadka <aadar.kdk@gmail.com>
2026-05-03 20:50:50 +0545

    install apache2 web server

--- 02-Scripting-Automation/scripts/bash-scripts/02-install_apache2.sh
+++ 02-Scripting-Automation/scripts/bash-scripts/02-install_apache2.sh
@@ -12,7 +12,7 @@ sudo systemctl start apache2
 
 # sudo systemctl enable apache2 #This line ensures apache2 starts on when machine boots.
 
-echo " Hello DevOps World! " > /var/www/html/index.html
+echo " Hello DevOps World! " | sudo tee /var/www/html/index.html > /dev/null
 
 sudo systemctl restart apache2
 
