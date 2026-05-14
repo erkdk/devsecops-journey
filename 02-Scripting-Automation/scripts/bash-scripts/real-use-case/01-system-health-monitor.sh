59b7d3a5da03dd1126d332024d26ec9523638615
Aadarsha Khadka <aadar.kdk@gmail.com>
2026-05-14 11:46:21 +0545

    monitor system health

---
+++ 02-Scripting-Automation/scripts/bash-scripts/real-use-case/01-system-health-monitor.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+echo "=== SYSTEM HEALTH ==="
+
+echo " "
+echo "Uptime:"
+uptime
+
+echo " "
+echo "Memory usage:"
+free -h 
+
+echo " "
+echo "Disk usage:"
+df -h
+
+echo " "
+echo "CPU Load: "
+top -bn1 | grep "load average"
+
+echo " "
+echo " Logged in users: "
+w
