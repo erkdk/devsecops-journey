15c2bf522707569598d541da8800fc9565884785
Aadarsha Khadka <aadar.kdk@gmail.com>
2026-05-17 17:25:06 +0545

    refactor: restructure AWS SAA-C03 IAM Terraform lab

---
+++ 10-aws/AWS-SAA-C03/terraform-labs/01-iam-lab/aws-env.example.sh
@@ -0,0 +1,10 @@
+###
+# Get the values from the AWS Details page for the user you created for this lab or
+# From the AWS CLI if you have it configured with the appropriate credentials of your own AWS account. 
+# Do not share these values with anyone else as they provide access to your AWS account.
+###
+
+export AWS_ACCESS_KEY_ID="REAL_KEY"  
+export AWS_SECRET_ACCESS_KEY="REAL_SECRET"
+export AWS_SESSION_TOKEN="REAL_TOKEN"
+export AWS_DEFAULT_REGION="us-east-1"
